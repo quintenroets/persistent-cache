de6d3f56f44d25dc398eb20828b02457833c48b3
Quinten <quinten.roets@gmail.com>
2025-01-14 23:48:47 -0800

    support specifying argument names to include in cache key

--- src/persistent_cache/main/decorator.py
+++ src/persistent_cache/main/decorator.py
@@ -1,4 +1,5 @@
-from collections.abc import Callable
+import inspect
+from collections.abc import Callable, Iterator
 from functools import wraps
 from typing import Any, cast, overload
 
@@ -12,8 +13,11 @@ from .cacheslot import CacheSlot
 def cache(
     function: F,
     *,
-    key_reducer: type[Reducer] = Reducer,
     cache_path: Path = Path.cache,
+    cache_key_arguments: tuple[str, ...] | str | None = None,
+    key_reducer: type[Reducer] = Reducer,
+    deep_learning: bool = False,
+    speedup_deep_learning: bool = False,
 ) -> F: ...
 
 
@@ -21,16 +25,22 @@ def cache(
 def cache(
     function: None = None,
     *,
-    key_reducer: type[Reducer] = Reducer,
     cache_path: Path = Path.cache,
+    cache_key_arguments: tuple[str, ...] | str | None = None,
+    key_reducer: type[Reducer] = Reducer,
+    deep_learning: bool = False,
+    speedup_deep_learning: bool = False,
 ) -> Callable[[F], F]: ...
 
 
-def cache(
+def cache(  # noqa: PLR0913
     function: F | None = None,
     *,
-    key_reducer: type[Reducer] = Reducer,
     cache_path: Path = Path.cache,
+    cache_key_arguments: tuple[str, ...] | str | None = None,
+    key_reducer: type[Reducer] | None = None,
+    deep_learning: bool = False,
+    speedup_deep_learning: bool = False,
 ) -> F | Callable[[F], F]:
     """A decorator to cache function results. Decorated functions are only executed if
     result is not present in cache. The arguments of the function can be any nested
@@ -45,10 +55,19 @@ def cache(
         ...
     """
 
+    reducer = extract_reducer(
+        key_reducer,
+        deep_learning=deep_learning,
+        speedup_deep_learning=speedup_deep_learning,
+    )
+
     def cache_decorator(function: F) -> F:
         @wraps(function)
         def wrapped_function(*args: Any, **kwargs: Any) -> Any:
-            cache_slot = CacheSlot(function, args, kwargs, key_reducer, cache_path)
+            arguments = tuple(
+                extract_argument_values(function, args, kwargs, cache_key_arguments),
+            )
+            cache_slot = CacheSlot(function, arguments, reducer, cache_path)
             try:
                 value = cache_slot.value
             except KeyError:
@@ -61,3 +80,43 @@ def cache(
     if function is not None:
         cache_decorator = cache_decorator(function)
     return cache_decorator
+
+
+def extract_argument_values(
+    function: F,
+    args: tuple[Any, ...],
+    kwargs: dict[str, Any],
+    cache_key_arguments: tuple[str, ...] | str | None = None,
+) -> Iterator[Any]:
+    if cache_key_arguments is None:
+        yield from (args, kwargs)
+    else:
+        arguments = inspect.signature(function).bind(*args, **kwargs)
+        arguments.apply_defaults()
+        if isinstance(cache_key_arguments, str):
+            yield arguments.arguments.get(cache_key_arguments)
+        else:
+            for name in cache_key_arguments:
+                yield arguments.arguments.get(name)
+
+
+def extract_reducer(
+    reducer: type[Reducer] | None,
+    *,
+    deep_learning: bool,
+    speedup_deep_learning: bool,
+) -> type[Reducer]:
+    if reducer is None:
+        if deep_learning:
+            from persistent_cache.reducers.deep_learning import Reducer as Reducer_
+
+            reducer = Reducer_
+        elif speedup_deep_learning:
+            from persistent_cache.reducers.speedup_deep_learning import (
+                Reducer as Reducer_,
+            )
+
+            reducer = Reducer_
+        else:
+            reducer = Reducer
+    return reducer
