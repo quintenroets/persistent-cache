5fe18a009c92bc1ef4f4b3b43deac3f67a665b68
Quinten <quinten.roets@gmail.com>
2025-04-09 03:54:29 +0000

    pre-commit auto updates

--- src/persistent_cache/models/path.py
+++ src/persistent_cache/models/path.py
@@ -2,6 +2,7 @@ from typing import TypeVar, cast
 
 import superpathlib
 from simple_classproperty import classproperty
+from typing_extensions import Self
 
 T = TypeVar("T", bound="Path")
 
@@ -9,11 +10,23 @@ T = TypeVar("T", bound="Path")
 class Path(superpathlib.Path):
     @classmethod
     @classproperty
-    def source_root(cls: type[T]) -> T:
+    def source_root(cls) -> Self:
         return cls(__file__).parent.parent
 
     @classmethod
     @classproperty
+<<<<<<< HEAD
     def cache(cls: type[T]) -> T:
         path = cls.script_assets / cls.source_root.name
         return cast(T, path)
+=======
+    def assets(cls) -> Self:
+        path = cls.script_assets / cls.source_root.name
+        return cast("Self", path)
+
+    @classmethod
+    @classproperty
+    def config(cls) -> Self:
+        path = cls.assets / "config" / "config.yaml"
+        return cast("Self", path)
+>>>>>>> template
